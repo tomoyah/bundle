9661b9829e94f1b3843e562b823cbf3c182ccb65
Cfearghost <cbalaki@college.harvard.edu>
2017-04-12 17:05:05 -0400

    Message Working; profile formatted

--- indexjs.js
+++ indexjs.js
@@ -4,21 +4,34 @@ var messageList = {
     ]
 }
 
-window.onbeforeunload = function() {
-    localStorage.setItem('Stickys', JSON.stringify(StickyNoteList.newMessages));
+
+function match(n) {
+    var buttonHTML = "";
+    buttonHTML += '<a class="ui-btn" href="#profile' + n + '">Profile' + n + '</a>';
+    $("#allprevMatches").html(buttonHTML);
+   /* var content = document.createElement('div');
+    content.innerHTML = 'class="ui-btn" href="#profile' + n + '">Profile ' + n;
+    //document.getElementById('allprevMatches').appendChild(content);
+    content += '<a class="ui-btn" href="#profile' + n + '">Profile' + n + '</a>';
+    $("#allprevMatches").appendChild(content);*/
 }
 
-function displayMessage(){
-
-}
-
-function submitMessage() {
-    $("listName").html(messageList.listname);
+function submitMessage(n) {
+    /*$("listName").html(messageList.listname);
     var output = document.getElementById("msgarea").value;
     messageList.newMessages.push(output)
     var messageHtml = "";
-    messageHtml += '<div data-inline="true">' + messageList.newMessages + '/>';
-    $("#allMsgs").html(messageHtml);
-
+    messageHtml += '<div data-inline="true">' + messageList.newMessages + '</div>';
+    $("#allMsgs").html(messageHtml);*/
+    var number = n
+    var stringnumber = number.toString()
+    var string1 = "msgarea"
+    var res1 = string1.concat(stringnumber)
+    var output = document.getElementById(res1).value;
+    var content = document.createElement('div');
+    content.innerHTML = 'You:' + output;
+    var string2 = "allMsgs"
+    var res2 = string2.concat(stringnumber)
+    document.getElementById(res2).appendChild(content);
 }
 
