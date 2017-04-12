5b473348b25407bc464258943ea70067ef03ebad
Cfearghost <cbalaki@college.harvard.edu>
2017-04-12 19:43:10 -0400

    Dynammic Messaging

--- indexjs.js
+++ indexjs.js
@@ -6,6 +6,8 @@ var messageList = {
 
 var profileList = ['David','Christi','Tomo'];
 var matchedWith = [false,false,false];
+var messaged = [false,false,false];
+
 
 function match(n) {
     var buttonHTML = "";
@@ -25,6 +27,9 @@ function toggleUnmatch(profileIndex){
   }else{
     $("#buttonProfile"+ profileIndex.toString()).html('Match');
     matchedWith[profileIndex-1] = false;
+    var buttonHTML = "";
+    buttonHTML += '';
+    $("#allprevMatches").html(buttonHTML);
   }
 }
 
@@ -35,19 +40,31 @@ function submitMessage(n) {
     var messageHtml = "";
     messageHtml += '<div data-inline="true">' + messageList.newMessages + '</div>';
     $("#allMsgs").html(messageHtml);*/
-    var number = n
-    var stringnumber = number.toString()
-    var string1 = "msgarea"
-    var res1 = string1.concat(stringnumber)
+    var number = n;
+    var stringnumber = number.toString();
+    var string1 = "msgarea";
+    var res1 = string1.concat(stringnumber);
     var output = document.getElementById(res1).value;
     var content = document.createElement('div');
     content.innerHTML = 'You:' + output;
-    var string2 = "allMsgs"
-    var res2 = string2.concat(stringnumber)
+    var string2 = "allMsgs";
+    var res2 = string2.concat(stringnumber);
     document.getElementById(res2).appendChild(content);
 }
 
 function search(){
   var search_results_html = '<a class="ui-btn" href="#profile1"><div class="ui-grid-a"><div class="ui-block-a"><img src="pics/t1.jpg" alt="profile1" width="80px"></div><div class="ui-block-b"><h1>David</h1>2 miles away</div></div></a><a class="ui-btn" href="#profile2"><div class="ui-grid-a"><div class="ui-block-a"><img src="pics/t2.jpg" alt="profile2" width="80px"></div><div class="ui-block-b"><h1>Christi</h1>1 mile away</div></div></a><a class="ui-btn" href="#profile3"><div class="ui-grid-a"><div class="ui-block-a"><img src="pics/t3.jpg" alt="profile3" width="80px"></div><div class="ui-block-b"><h1>Tomo</h1>3 miles away</div></div></a></div>';
   $(".search-results").html(search_results_html);
+}
+
+function message(messageIndex){
+    var msgHTML = "";
+    messaged[messageIndex-1] = true;
+    for (i = 0; i < messaged.length; i++){
+        if (messaged[i] == true){
+            var num = i+1
+            msgHTML += '<div> <img src="pics/t' + num + '.jpg" alt="profile" width="50px"> <a href="#profile' + num + '">' + profileList[i] +'</a> <a data-inline="true" data-role="button" href="#chat' + num + '" >Chat</a> </div>'
+        }
+    }
+    $("#previousMessages").html(msgHTML);
 }
